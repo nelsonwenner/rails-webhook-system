b3cf8fe0f3fe6d0ee5ee918a757b0063149aee51
Nelson Dias <nelsondiasdemedeiros@gmail.com>
2022-05-31 22:49:31 -0300

    Add webhook worker

--- app/workers/webhook_worker.rb
+++ app/workers/webhook_worker.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'http'
+require "http"
 
 class WebhookWorker < ApplicationWorker
   class FailedRequestError < StandardError; end
@@ -9,46 +9,41 @@ class WebhookWorker < ApplicationWorker
     @event_id = event_id
   end
 
-  def perform
+  def call
     return if webhook_event.blank? || webhook_endpoint.blank?
 
-    # Send the webhook request with a 30 second timeout.
-    response = HTTP.timeout(30)
-      .headers(
-        'User-Agent': 'webhook_system/1.0',
-        'Content-Type': 'application/json'
-      )
-      .post(
-        webhook_endpoint.url,
-        body: {
-          event: webhook_event.event,
-          payload: webhook_event.payload
-        }.to_json
-      )
-
-    # Store the webhook response
     webhook_event.update(response: {
-                           headers: response.headers.to_h,
-                           code: response.code.to_i,
-                           body: response.body.to_s
+                           headers: fetch_response.headers.to_h,
+                           code: fetch_response.code.to_i,
+                           body: fetch_response.body.to_s
                          })
 
-    raise FailedRequestError unless response.status.success?
+    raise FailedRequestError unless fetch_response.status.success?
   rescue HTTP::TimeoutError
-    webhook_event.update(response: { error: 'TIMEOUT_ERROR' })
+    webhook_event.update(response: { error: "TIMEOUT_ERROR" })
   rescue HTTP::ConnectionError
-    webhook_event.update(response: { error: 'CONNECTION_ERROR' })
+    webhook_event.update(response: { error: "CONNECTION_ERROR" })
   end
 
   private
 
-    attr_reader :webhook_event, :webhook_endpoint
+    def fetch_response
+      # Request with a 30 second timeout.
+      @fetch_response ||= HTTP.timeout(30).headers(
+        "User-Agent": "webhook_system/1.0",
+        "Content-Type": "application/json"
+      ).post(webhook_endpoint.url,
+             body: {
+               event: webhook_event.event,
+               payload: webhook_event.payload
+             }.to_json)
+    end
 
     def webhook_event
       @webhook_event ||= WebhookEvent.find_by(id: @event_id)
     end
 
     def webhook_endpoint
-      @webhook_endpoint ||= webhook_event.webhook_endpoint
+      @webhook_endpoint ||= webhook_event&.webhook_endpoint
     end
 end
