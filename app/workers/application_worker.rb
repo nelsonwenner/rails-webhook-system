bb343ecf01915044fcbe9ab0afcbb6a9700d31e9
Nelson Dias <nelsondiasdemedeiros@gmail.com>
2022-06-04 18:15:08 -0300

    Add config sidekiq

--- app/workers/application_worker.rb
+++ app/workers/application_worker.rb
@@ -2,4 +2,14 @@
 
 class ApplicationWorker
   include Sidekiq::Worker
+
+  sidekiq_options retry: 10, dead: false
+
+  sidekiq_retry_in do |retry_count| 
+    # Exponential backoff, with a random 30-second to 10-minute "jitter"   
+    # added in to help spread out any webhook "bursts."                    
+    jitter = rand(30.seconds..10.minutes).to_i 
+ 
+    (retry_count ** 5) + jitter 
+  end 
 end
