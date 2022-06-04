95607a10b8d47a037024698c37f3438ce8d84d1f
Nelson Dias <nelsondiasdemedeiros@gmail.com>
2022-06-03 22:31:42 -0300

    Add a new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => '/sidekiq'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  post '/webhooks', to: proc { [204, {}, []] }
 end
