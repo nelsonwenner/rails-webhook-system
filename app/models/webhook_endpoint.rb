a338a83392d03f4938e332113e2c8911278579be
Nelson Dias <nelsondiasdemedeiros@gmail.com>
2022-06-04 12:52:50 -0300

    Check if the event is subscribed

--- app/models/webhook_endpoint.rb
+++ app/models/webhook_endpoint.rb
@@ -3,5 +3,10 @@
 class WebhookEndpoint < ApplicationRecord
   has_many :webhook_events, dependent: :destroy
 
+  validates :subscriptions, length: { minimum: 1 }, presence: true
   validates :url, presence: true
+
+  def subscribed?(event:)
+    (subscriptions & ['*', event]).any?
+  end
 end
--- db/migrate/20220528183318_create_webhook_endpoints.rb
+++ db/migrate/20220528183318_create_webhook_endpoints.rb
@@ -2,6 +2,7 @@ class CreateWebhookEndpoints < ActiveRecord::Migration[7.0]
   def change
     create_table :webhook_endpoints do |t|
       t.string :url, null: false
+      t.jsonb :subscriptions, default: ['*']
 
       t.timestamps null: false
     end
